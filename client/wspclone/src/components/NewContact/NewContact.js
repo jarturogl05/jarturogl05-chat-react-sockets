15f2f066f5f2607fa2367a03b463861e09b15364
jarturogl05 <jarturogl05@gmail.com>
2021-04-04 14:18:58 -0500

    newcontact styles

--- client/wspclone/src/components/NewContact/NewContact.js
+++ client/wspclone/src/components/NewContact/NewContact.js
@@ -1,10 +1,43 @@
-import React from 'react'
-import './NewContact.css'
+import React, { useRef } from "react";
+import "./NewContact.css";
 
-export default function NewContact() {
-    return (
-        <div>
-            a
-        </div>
-    )
+export default function NewContact({closeModal}) {
+  const idRef = useRef();
+  const nameRef = useRef();
+
+    function handleSumbit(e){
+        e.preventDefault();
+
+        closeModal();
+    }
+
+  return (
+    <div>
+      <h2>Add new contact</h2>
+
+      <form className="newContact_form" onSubmit={handleSumbit}>
+        <label htmlFor="newcontact_id">Id</label>
+        <input
+          className=""
+          id="newcontact_id"
+          type="text"
+          required
+          ref={idRef}
+        ></input>
+
+        <label htmlFor="newcontact_name">Name</label>
+        <input
+          className=""
+          id="newcontact_name"
+          type="text"
+          required
+          ref={nameRef}
+        ></input>
+
+        <button className="NewContact_button" type="submit">
+          ADD
+        </button>
+      </form>
+    </div>
+  );
 }
