ed99cf2d464f4aa1805220fe9807c9030ff6ba7d
jarturogl05 <jarturogl05@gmail.com>
2021-04-07 19:43:32 -0500

    new openconversation component

--- client/wspclone/src/pages/Dashboard/Dashboard-page.js
+++ client/wspclone/src/pages/Dashboard/Dashboard-page.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar/Sidebar'
+import OpenConversation from '../../components/OpenConversation/OpenConversation'
 import './Dashboard.css'
+import { useConversations } from '../../context/ConversationsProvider'
 
 export default function Dashboard({id}) {
+    const {selectedConversation} = useConversations()
+
     return (
         <div className='dashboard-container'>
             <Sidebar id={id}/>
             <div className='contenido'>
+                {selectedConversation && <OpenConversation/>}
             </div>
         </div>
     )
